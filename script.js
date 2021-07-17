50945d2efacc6ade8e57c4e47f8f7894ed497d78
azs6189 <80870807+azs6189@users.noreply.github.com>
2021-07-17 09:10:00 -0400

    JS update: questions and answers

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+
+"Commonly used data types DO NOT include:"
+strings
+booleans
+alerts
+numbers
+
+"The condition in an if/else statement is enclosed within:"
+quotes
+curly brackets
+parenthesis
+square brackets
+
+"Arrays in JavaScript can be used to store ___."
+numbers and strings
+other arrays
+booleans
+all of the above
+
+"String values must be enclosed within ___ when being assigned to variables."
+commas
+curly brackets
+quotes
+parenthesis
+
+"A very useful tool used during development and debugging for printing content to the debugger is:"
+JavaScript
+terminal / bash
+for loops
+consol log
